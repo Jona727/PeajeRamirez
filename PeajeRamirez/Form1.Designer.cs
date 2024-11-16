ac346779a23569ded9469021a7d704c678df79e5
Windows <Windows@DESKTOP-VRB467I>
2024-11-15 21:03:01 -0300

    Agregar archivos de proyecto.

---
+++ PeajeRamirez/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace PeajeRamirez
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            groupBox1 = new GroupBox();
+            pictureBox1 = new PictureBox();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            groupBox2 = new GroupBox();
+            button1 = new Button();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Silver;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.FlatStyle = FlatStyle.Flat;
+            label1.Font = new Font("Lucida Console", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(204, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(357, 26);
+            label1.TabIndex = 0;
+            label1.Text = "REGISTRO DE AUTOMÓVILES";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Silver;
+            label2.Location = new Point(88, 31);
+            label2.Name = "label2";
+            label2.Size = new Size(90, 15);
+            label2.TabIndex = 1;
+            label2.Text = "FECHA Y HORA";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Silver;
+            label3.Location = new Point(129, 67);
+            label3.Name = "label3";
+            label3.Size = new Size(49, 15);
+            label3.TabIndex = 2;
+            label3.Text = "MARCA";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Silver;
+            label4.Location = new Point(35, 105);
+            label4.Name = "label4";
+            label4.Size = new Size(143, 15);
+            label4.TabIndex = 3;
+            label4.Text = "CANTIDAD DE PERSONAS";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Silver;
+            label5.Location = new Point(267, 105);
+            label5.Name = "label5";
+            label5.Size = new Size(53, 15);
+            label5.TabIndex = 4;
+            label5.Text = "PATENTE";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Silver;
+            label6.Location = new Point(434, 105);
+            label6.Name = "label6";
+            label6.Size = new Size(46, 15);
+            label6.TabIndex = 5;
+            label6.Text = "CARRIL";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.Silver;
+            textBox2.Location = new Point(326, 101);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(85, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.Silver;
+            textBox3.Location = new Point(486, 100);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(78, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = Color.Silver;
+            textBox4.Location = new Point(205, 59);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(363, 23);
+            textBox4.TabIndex = 2;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(205, 23);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.ForestGreen;
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label6);
+            groupBox1.ForeColor = SystemColors.ActiveCaptionText;
+            groupBox1.Location = new Point(12, 38);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(753, 178);
+            groupBox1.TabIndex = 11;
+            groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Silver;
+            pictureBox1.Location = new Point(583, 22);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(154, 144);
+            pictureBox1.TabIndex = 17;
+            pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.Silver;
+            textBox1.Location = new Point(205, 102);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(45, 23);
+            textBox1.TabIndex = 16;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(267, 129);
+            button2.Name = "button2";
+            button2.Size = new Size(157, 37);
+            button2.TabIndex = 14;
+            button2.Text = "AGREGAR";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 287);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(753, 219);
+            dataGridView1.TabIndex = 12;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button1);
+            groupBox2.Controls.Add(textBox5);
+            groupBox2.Controls.Add(label7);
+            groupBox2.Location = new Point(12, 222);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(753, 49);
+            groupBox2.TabIndex = 13;
+            groupBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(564, 10);
+            button1.Name = "button1";
+            button1.Size = new Size(173, 32);
+            button1.TabIndex = 16;
+            button1.Text = "BUSCAR";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox5
+            // 
+            textBox5.BackColor = Color.Silver;
+            textBox5.Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            textBox5.Location = new Point(129, 19);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(415, 23);
+            textBox5.TabIndex = 15;
+            textBox5.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.LightGray;
+            label7.Location = new Point(35, 19);
+            label7.Name = "label7";
+            label7.Size = new Size(51, 15);
+            label7.TabIndex = 14;
+            label7.Text = "BUSCAR";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.ForestGreen;
+            ClientSize = new Size(788, 508);
+            Controls.Add(groupBox2);
+            Controls.Add(dataGridView1);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private DateTimePicker dateTimePicker1;
+        private GroupBox groupBox1;
+        private DataGridView dataGridView1;
+        private Button button2;
+        private GroupBox groupBox2;
+        private Button button1;
+        private TextBox textBox5;
+        private Label label7;
+        private PictureBox pictureBox1;
+        private TextBox textBox1;
+    }
+}
---
+++ PeajeRamirez/Form1.cs
@@ -0,0 +1,12 @@
+namespace PeajeRamirez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
